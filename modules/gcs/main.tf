3b3969978e26f41aee8db4168b167a621aef7172
Achyut Sharma <sharma.achyut17@gmail.com>
2025-04-25 15:13:49 +0000

    Instructional Updates

--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -1,3 +1,7 @@
+locals {
+  network = "${element(split("-", var.subnet), 0)}"
+}
+
 # Resource to generate a random suffix
 resource "random_id" "bucket_suffix" {
   # byte_length determines the randomness. 4 bytes = 8 hex characters.
@@ -9,7 +13,7 @@ resource "random_id" "bucket_suffix" {
 resource "google_storage_bucket" "basic_bucket" {
   # --- Required Arguments ---
   # Bucket names must be globally unique across all of Google Cloud
-  name = "${random_id.bucket_suffix.hex}"
+  name = "${local.network}"-"${random_id.bucket_suffix.hex}"
 
   # The location (region, multi-region, dual-region) for the bucket
   location = "US-CENTRAL1" # Example: A single region
