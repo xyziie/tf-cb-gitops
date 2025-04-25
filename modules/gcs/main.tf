680db1be4b7adc694e4802682f19570a9753deec
Achyut Sharma <sharma.achyut17@gmail.com>
2025-04-25 15:17:36 +0000

    Instructional Updates

--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -13,7 +13,7 @@ resource "random_id" "bucket_suffix" {
 resource "google_storage_bucket" "basic_bucket" {
   # --- Required Arguments ---
   # Bucket names must be globally unique across all of Google Cloud
-  name = "${local.network}"-"${random_id.bucket_suffix.hex}"
+  name = "${local.network}-${random_id.bucket_suffix.hex}"
 
   # The location (region, multi-region, dual-region) for the bucket
   location = "US-CENTRAL1" # Example: A single region
